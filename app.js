4916f6e63b9db4fc22e883ac3716fed3abe4b378
NatClamp <nat.clamp@gmail.com>
2018-12-03 20:43:15 +0000

    sorting out error in node_modules and making scripts in package.json, creating router

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 const bodyParser = require('body-parser');
+const router = require('./routes/api');
 
 app.use(bodyParser.json());
+
+app.use('/api', router);
 
 app.all('/*', (req, res, next) => {
   res.status(404).send({ msg: 'Sorry, path does not exist' });
