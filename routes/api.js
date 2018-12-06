4f1f81d888a0f3d38823408396992aface69f70f
NatClamp <nat.clamp@gmail.com>
2018-12-06 20:18:11 +0000

    added catch to check if user_id in parameters is a number, if not, error before it goes to any of the endpoints

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,12 @@ const topicsRouter = require('./topics');
 const articlesRouter = require('./articles');
 const usersRouter = require('./users');
 const { getEndpointJSON } = require('../controllers/api');
+const { handle405 } = require('../errors/index');
 
 router
   .route('/')
-  .get(getEndpointJSON);
+  .get(getEndpointJSON)
+  .all(handle405);
 
 router.use('/topics', topicsRouter);
 router.use('/articles', articlesRouter);
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const { getAllUsers, getSingleUser } = require('../controllers/users');
 const { handle405 } = require('../errors/index');
 
+router.param('user_id', (req, res, next, user_id) => {
+  if (Number.isInteger(+user_id)) next();
+  else next({ code: '22P02' });
+});
+
 router
   .route('/')
   .get(getAllUsers)
