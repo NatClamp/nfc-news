bed7d937cf71d28a329aa58604059c535aeb1cff
NatClamp <nat.clamp@gmail.com>
2018-12-07 14:34:18 +0000

    sorting out production database connection

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,18 @@
 // Update with your config settings.
 
+const { DB_URL } = process.env;
+
 module.exports = {
+  production: {
+    client: 'pg',
+    connection: `${DB_URL}?ssl=true`,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seed',
+    },
+  },
   development: {
     client: 'pg',
     connection: {
--- listen.js
+++ listen.js
@@ -2,8 +2,9 @@
 
 const app = require('./app');
 
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, (err) => {
+app.listen(PORT, (err) => {
   if (err) console.log(err);
-  else console.log('listening on 9090');
+  else console.log(`listening on ${PORT}`);
 });
