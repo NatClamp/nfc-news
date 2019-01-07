5dabfadd5640b5b8f9beb452b140a982fbd8d222
NatClamp <nat.clamp@gmail.com>
2019-01-07 11:47:32 +0000

    adding cors

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const bodyParser = require('body-parser');
 const router = require('./routes/api');
 const {
-  handle404, handle422, handle400, handle500,
+  handle404,
+  handle422,
+  handle400,
+  handle500,
 } = require('./errors/index');
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use('/api', router);
 
