1a65ebe60e08fe8ffa8e7231d1ab9106d0b355f3
NatClamp <nat.clamp@gmail.com>
2019-01-09 21:47:21 +0000

    fix linting

--- models/users.js
+++ models/users.js
@@ -1,15 +1,15 @@
 const connection = require('../db/connection');
 
 exports.getUsers = (req, res, next) => connection('users')
-    .select('*')
-    .modify((userQuery) => {
-      if (req.params.username) userQuery.where('username', req.params.username);
-    })
-    .then((users) => {
-      if (users.length === 0) {
-        return next({ status: 404, message: 'Page not found' });
-      }
-      if (users.length === 1) [users] = users;
-      return res.status(200).send({ users });
-    });
-// .catch(next);
+  .select('*')
+  .modify((userQuery) => {
+    if (req.params.username) userQuery.where('username', req.params.username);
+  })
+  .then((users) => {
+    if (users.length === 0) {
+      return next({ status: 404, message: 'Page not found' });
+    }
+    if (users.length === 1) [users] = users;
+    return res.status(200).send({ users });
+  })
+  .catch(next);
